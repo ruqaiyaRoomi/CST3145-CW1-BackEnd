9f5f4f690656749ad4927f64e7a173acd529be5a
RuqaiyaRoomi <ruqaiyaroomi.com>
2025-11-20 16:25:01 +0400

    edited connection string

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 let db;
 
 MongoClient.connect(
-  'mongodb+srv://ruqaiyah:RR1026@cw1.u4ssebh.mongodb.net',
+  'mongodb+srv://ruqaiyah:RR1026@cw1.u4ssebh.mongodb.net/?appName=CW1',
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
     if (err) {
